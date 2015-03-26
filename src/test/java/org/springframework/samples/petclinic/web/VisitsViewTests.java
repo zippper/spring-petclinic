faf0e141b20e9ae50f4450567f92bdeaa476f630
Nauris <zippper@inbox.lv>
2015-03-26 23:29:21 +0200

    Test asd

--- src/test/java/org/springframework/samples/petclinic/web/VisitsViewTests.java
+++ src/test/java/org/springframework/samples/petclinic/web/VisitsViewTests.java
@@ -36,7 +36,7 @@ import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-//comment aaaabbbb
+//comment aaaabbbbccccc
 /**
  * @author Arjen Poutsma
  * @author Michael Isvy
